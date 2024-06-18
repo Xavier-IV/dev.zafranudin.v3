bad85f2c3f1f88be7f3dfbdf97e9620f7300ca2d
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2024-06-18 13:58:35 +0200

    feat: Restructure

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -52,14 +52,18 @@ export default function Home() {
         <h1 className="text-4xl md:text-7xl">Zafranudin Zafrin</h1>
         <h2 className="text-lg font-light">a software craftsman.</h2>
       </div>
-      <div className="flex flex-col items-center gap-6 px-4 md:px-10 pt-20 pb-20 bg-gradient-to-t from-zinc-800 from-0% via-black via-30% w-full border-b border-b-zinc-800">
+      <div className="flex flex-col items-center gap-6 px-4 md:px-10 pt-20 pb-20 bg-gradient-to-t from-zinc-800 from-0% via-black via-30% w-full border-b-2 border-b-zinc-500">
         <div className="flex flex-col gap-10 text-2xl">
           <Button size="lg" variant="solid" className="text-white fill-zinc-50">
             Connect <RegularLinkedinOriginal className="text-lg " />
           </Button>
 
-          <div className="fill-zinc-50 flex gap-4 items-center justify-center">
-            <a href="https://github.com/Xavier-IV" target="_blank">
+          <div className="fill-zinc-100 flex gap-4 items-center justify-center">
+            <a
+              href="https://github.com/Xavier-IV"
+              className="fill-zinc-100"
+              target="_blank"
+            >
               <RegularGithubOriginal />
             </a>
             <a
@@ -89,102 +93,104 @@ export default function Home() {
           <CardList />
         </div>
       </div>
-      <div className="max-w-6xl mx-auto mt-10 px-4 md:px-10 flex flex-col text-center gap-10 items-center justify-center w-full">
-        <h2 className="text-3xl font-semibold">Technical Experience</h2>
+      <div className="mx-auto mt-10 px-4 md:px-10 flex flex-col text-center gap-10 items-center justify-center w-full border-b-2 border-b-zinc-500">
+        <div className="mt-10 flex flex-col gap-10">
+          <h2 className="text-3xl font-semibold">Technical Experience</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-          <div className="flex flex-col gap-4">
-            <h3 className="text-xl">Cloud &amp; Deployment</h3>
-            <p>{clouds.map((cloud) => cloud.name).join(", ")}</p>
-            <div className="flex gap-2 items-center justify-center fill-white">
-              {clouds.map((cloud) => (
-                <div className="bg-white rounded-md">
-                  <Tooltip content={cloud.name} className="bg-zinc-800">
-                    <Image
-                      src={cloud.icon}
-                      alt="GCP Logo"
-                      className="w-10 p-2"
-                    />
-                  </Tooltip>
-                </div>
-              ))}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+            <div className="flex flex-col gap-4">
+              <h3 className="text-xl">Cloud &amp; Deployment</h3>
+              <p>{clouds.map((cloud) => cloud.name).join(", ")}</p>
+              <div className="flex gap-2 items-center justify-center fill-white">
+                {clouds.map((cloud) => (
+                  <div className="bg-white rounded-md">
+                    <Tooltip content={cloud.name} className="bg-zinc-800">
+                      <Image
+                        src={cloud.icon}
+                        alt="GCP Logo"
+                        className="w-10 p-2"
+                      />
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <h3 className="text-xl">Frontend Technologies</h3>
+              <p>{frontends.map((frontend) => frontend.name).join(", ")}</p>
+              <div className="flex gap-2 items-center justify-center fill-white">
+                {frontends.map((frontend) => (
+                  <div className="bg-white rounded-md">
+                    <Tooltip content={frontend.name} className="bg-zinc-800">
+                      <Image
+                        src={frontend.icon}
+                        alt="GCP Logo"
+                        className="w-10 p-2"
+                      />
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <h3 className="text-xl">Backend Technologies</h3>
+              <p>{backends.map((backend) => backend.name).join(", ")}</p>
+              <div className="flex gap-2 items-center justify-center fill-white">
+                {backends.map((backend) => (
+                  <div className="bg-white rounded-md">
+                    <Tooltip content={backend.name} className="bg-zinc-800">
+                      <Image
+                        src={backend.icon}
+                        alt="GCP Logo"
+                        className="w-10 p-2"
+                      />
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <h3 className="text-xl">Programming Languages</h3>
+              <p>{languages.map((language) => language.name).join(", ")}</p>
+              <div className="flex gap-2 items-center justify-center fill-white">
+                {languages.map((language) => (
+                  <div className="bg-white rounded-md">
+                    <Tooltip content={language.name} className="bg-zinc-800">
+                      <Image
+                        src={language.icon}
+                        alt="GCP Logo"
+                        className="w-10 p-2"
+                      />
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="col-span-1 md:col-span-2 flex flex-col gap-4">
+              <h3 className="text-xl">Database</h3>
+              <p>{databases.map((database) => database.name).join(", ")}</p>
+              <div className="flex gap-2 items-center justify-center fill-white">
+                {databases.map((database) => (
+                  <div className="bg-white rounded-md">
+                    <Tooltip content={database.name} className="bg-zinc-800">
+                      <Image
+                        src={database.icon}
+                        alt="GCP Logo"
+                        className="w-10 p-2"
+                      />
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <h3 className="text-xl">Frontend Technologies</h3>
-            <p>{frontends.map((frontend) => frontend.name).join(", ")}</p>
-            <div className="flex gap-2 items-center justify-center fill-white">
-              {frontends.map((frontend) => (
-                <div className="bg-white rounded-md">
-                  <Tooltip content={frontend.name} className="bg-zinc-800">
-                    <Image
-                      src={frontend.icon}
-                      alt="GCP Logo"
-                      className="w-10 p-2"
-                    />
-                  </Tooltip>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <h3 className="text-xl">Backend Technologies</h3>
-            <p>{backends.map((backend) => backend.name).join(", ")}</p>
-            <div className="flex gap-2 items-center justify-center fill-white">
-              {backends.map((backend) => (
-                <div className="bg-white rounded-md">
-                  <Tooltip content={backend.name} className="bg-zinc-800">
-                    <Image
-                      src={backend.icon}
-                      alt="GCP Logo"
-                      className="w-10 p-2"
-                    />
-                  </Tooltip>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <h3 className="text-xl">Programming Languages</h3>
-            <p>{languages.map((language) => language.name).join(", ")}</p>
-            <div className="flex gap-2 items-center justify-center fill-white">
-              {languages.map((language) => (
-                <div className="bg-white rounded-md">
-                  <Tooltip content={language.name} className="bg-zinc-800">
-                    <Image
-                      src={language.icon}
-                      alt="GCP Logo"
-                      className="w-10 p-2"
-                    />
-                  </Tooltip>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="col-span-1 md:col-span-2 flex flex-col gap-4">
-            <h3 className="text-xl">Database</h3>
-            <p>{databases.map((database) => database.name).join(", ")}</p>
-            <div className="flex gap-2 items-center justify-center fill-white">
-              {databases.map((database) => (
-                <div className="bg-white rounded-md">
-                  <Tooltip content={database.name} className="bg-zinc-800">
-                    <Image
-                      src={database.icon}
-                      alt="GCP Logo"
-                      className="w-10 p-2"
-                    />
-                  </Tooltip>
-                </div>
-              ))}
-            </div>
-          </div>
+          <div className="mt-20"></div>
         </div>
-
-        <div className="mt-20"></div>
       </div>
     </div>
   );
